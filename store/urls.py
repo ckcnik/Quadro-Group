209628397dd4b5af9cb8e6588300db5982ec54ac
Денис Суховерхов <at10tion.angry@yandex.ru>
2016-02-15 14:27:54 +0300

    remove db.sqlite3 from git

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from .views import PopulationList
 
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^app/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^app/api/stat/$', PopulationList.as_view(), name='population'),
 ]
